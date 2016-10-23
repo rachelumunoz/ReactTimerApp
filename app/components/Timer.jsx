5928ce9629a3bbdc7e9a7d1bd7f3859ce6ef63f2
Rachel Munoz <rachelumunoz@gmail.com>
2016-10-22 18:04:07 -0700

    Adds nav and routes

---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react')
+
+var Timer = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h2>Timer Component!</h2>
+      </div>
+    )
+  }
+})
+
+module.exports = Timer
